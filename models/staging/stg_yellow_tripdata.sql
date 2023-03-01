ce32b1cd80aac5aad8c880736d7656c8ccb04943
patrick.pimentel5@gmail.com <patrick.pimentel5@gmail.com>
2023-03-01 15:52:47 +0000

    +yellow_data / var macro

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -4,21 +4,22 @@
 
 select
     -- identifiers
-    {{ dbt_utils.generate_surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
+    {{ dbt_utils.generate_surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
     cast(ratecodeid as integer) as ratecodeid,
     cast(pulocationid as integer) as  pickup_locationid,
     cast(dolocationid as integer) as dropoff_locationid,
     
     -- timestamps
-    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
-    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
     
     -- trip info
     store_and_fwd_flag,
     cast(passenger_count as integer) as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
-    cast(trip_type as integer) as trip_type,
+    -- yellow taxis são sempre "Street-hail" (não reservado e chamado na rua levantando a mão) -- 1 = Street-hail
+    1 as trip_type,
     
     -- payment info
     cast(fare_amount as numeric) as fare_amount,
@@ -26,13 +27,14 @@ select
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
-    cast(ehail_fee as numeric) as ehail_fee,
+    -- "E-hail fee" = taxa cobrada quando solicita taxi por aplicativo. Yellow taxis não possuem.
+    0 as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
-from  {{ source('staging','green_tripdata') }}
+from  {{ source('staging','yellow_tripdata') }}
 where vendorid is not null
 
 -- dbt run --select stg_green_tripdata --var 'is_test_run: false' to not limit to hundred
